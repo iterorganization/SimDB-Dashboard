e9a6714bbaf8218f2f6a302ade56cb2a24232e24
marood <deepakkumar.maroo@iter.org>
2025-04-01 15:13:11 +0200

    Updated version and minor alignment corrections

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -7,17 +7,17 @@ import { RouterView } from 'vue-router'
     <v-system-bar app dark color="#002F55">
       ITER Simulation Database
       <v-spacer></v-spacer>
-      Version 0.4
+      Version 0.5
     </v-system-bar>
 
     <v-app-bar app shrink-on-scroll dark color="#002F55" height="80" dense>
       <v-spacer></v-spacer>
-      <v-toolbar-title class="d-flex align-center" style="height: 100%">
+      <v-toolbar-title class="d-flex align-center justify-center" style="height: 100%">
         <v-btn
           variant="text"
           href="/dashboard"
           class="text-none text-h4"
-          style="letter-spacing: 0; color: white !important"
+          style="letter-spacing: 0; color: white !important;"
           >SimDB Dashboard</v-btn
         >
       </v-toolbar-title>
